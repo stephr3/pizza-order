96bac67936f45f52adfa0b4f1e844a6336e880bb
Stephanie Gurung <stephr3@gmail.com>
2016-06-17 08:38:50 -0700

    return a new pizza price based on pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 //Business Logic
-var Pizza = function (size, toppings, price) {
+//Set Objects
+function Pizza (size, toppings, price) {
   this.pizzaSize = size;
   this.pizzaToppings = toppings;
   this.pizzaPrice = price;
 };
+//Set Prototypes
+Pizza.prototype.calculatePrice = function () {
+  if (this.pizzaSize === "small"){
+    return this.pizzaPrice = 7;
+  } else if (this.pizzaSize === "medium") {
+    return this.pizzaPrice = 9;
+  } else {
+    return this.pizzaPrice = 11;
+  }
+}
