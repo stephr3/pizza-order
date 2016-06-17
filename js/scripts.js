b1193890989fd211163e74f46d36b8940b5f2b6f
Stephanie Gurung <stephr3@gmail.com>
2016-06-17 08:46:25 -0700

    add  to pizza priced for each extra topping ordered

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,18 @@ function Pizza (size, toppings, price) {
   this.pizzaPrice = price;
 };
 //Set Prototypes
-Pizza.prototype.calculatePrice = function () {
+Pizza.prototype.calculateSizePrice = function () {
   if (this.pizzaSize === "small"){
     return this.pizzaPrice = 7;
   } else if (this.pizzaSize === "medium") {
     return this.pizzaPrice = 9;
   } else {
     return this.pizzaPrice = 11;
+  }
+}
+
+Pizza.prototype.addToppings = function () {
+  for (i=0; i<this.pizzaToppings.length; i++) {
+    this.pizzaPrice += 1;
   }
 }
